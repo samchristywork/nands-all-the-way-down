cb9e3a39f9070f0d36ddf3f87b558aa435f285da
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-10-06 18:22:33 -0500

    feat: Add functions for serializing and deserializing gates
    These functions simply copy memory from the NandGate structure into a
    file, and back again.

--- src/serialization.c
+++ src/serialization.c
@@ -7,6 +7,35 @@ extern int nSubassemblies;
 extern NandGate **gates;
 extern int nGates;
 
+void serializeNand(FILE *f, NandGate *g) {
+  fwrite(&g->pos, sizeof(Vec2), 1, f);
+
+  fwrite(&g->nInputs, sizeof(int), 1, f);
+
+  if (g->nInputs > 0) {
+    fwrite(g->inputs, sizeof(int), g->nInputs, f);
+  }
+
+  fwrite(&g->value, sizeof(bool), 1, f);
+  fwrite(&g->nextValue, sizeof(bool), 1, f);
+}
+
+void deserializeNand(FILE *f, NandGate *g) {
+  fread(&g->pos, sizeof(Vec2), 1, f);
+
+  fread(&g->nInputs, sizeof(int), 1, f);
+
+  if (g->nInputs > 0) {
+    g->inputs = (int *)malloc(g->nInputs * sizeof(int));
+    fread(g->inputs, sizeof(int), g->nInputs, f);
+  } else {
+    g->inputs = NULL;
+  }
+
+  fread(&g->value, sizeof(bool), 1, f);
+  fread(&g->nextValue, sizeof(bool), 1, f);
+}
+
 void save() {
   printf("Saving\n");
 

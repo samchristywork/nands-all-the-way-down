a9d7424aaace52cfaeafb4f62ffe47e233dfee2c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-10-14 20:15:00 -0500

    feat: Add function to handle left mouse
    This function handles the left mouse press. This relies on a ton of
    global variables, to be changed later.

--- src/event.c
+++ src/event.c
@@ -33,3 +33,79 @@ typedef enum MODE {
 MODE mode;
 
 Port selection[2];
+
+bool handleLeftMouseEvents(SDL_Event e, Vec2 mouse) {
+  bool shouldRender=false;
+
+  if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT) {
+    panDragStart.x = -1;
+    panDragStart.y = -1;
+    clickPos.x = mouse.x;
+    clickPos.y = mouse.y;
+
+    if(mode==SELECT_CHILD) {
+      mode=NORMAL;
+      for (int i = 0; i < nSubassemblies; i++) {
+        float x = mouse.x;
+        float y = mouse.y;
+        Subassembly *s = subassemblies[i];
+        clickPos.x = s->rect.x - x;
+        clickPos.y = s->rect.y - y;
+        if (isInSubassembly(s, x, y)) {
+          if (subassemblies[i]!=currentSubassembly) {
+            addSubassembly(currentSubassembly, i);
+          }
+          break;
+        }
+      }
+    } else {
+      bool subassemblyClicked = false;
+      bool nandClicked = false;
+
+      for (int i = 0; i < nGates; i++) {
+        float x = mouse.x;
+        float y = mouse.y;
+        NandGate *g = gates[i];
+        clickPos.x = g->pos.x - x;
+        clickPos.y = g->pos.y - y;
+        if (checkGateCollision(g, (Vec2){x, y})) {
+          nandDrag = g;
+          nandClicked = true;
+          shouldRender = true;
+          selectedNand = g;
+          break;
+        }
+      }
+
+      if (!nandClicked) {
+        for (int i = 0; i < nSubassemblies; i++) {
+          float x = mouse.x;
+          float y = mouse.y;
+          Subassembly *s = subassemblies[i];
+          clickPos.x = s->rect.x - x;
+          clickPos.y = s->rect.y - y;
+          if (isInSubassembly(s, x, y)) {
+            subassemblyDrag = s;
+            subassemblyClicked = true;
+            shouldRender = true;
+            currentSubassembly = s;
+            break;
+          }
+        }
+      }
+
+      if (!subassemblyClicked && !nandClicked) {
+        isDraggingViewport = true;
+      }
+    }
+  }
+
+  if (e.type == SDL_MOUSEBUTTONUP && e.button.button == SDL_BUTTON_LEFT) {
+    subassemblyDrag = NULL;
+    nandDrag = NULL;
+    isDraggingViewport = false;
+    shouldRender = true;
+  }
+
+  return shouldRender;
+}
